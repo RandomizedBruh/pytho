be85edeaba29b8a21d18342291713122a79a4833
pivnoybochok <166494345+RandomizedBruh@users.noreply.github.com>
2024-12-23 18:28:43 +1000

    gamemem

---
+++ mygame/pytho-main/mygame/mygame.py
@@ -0,0 +1,26 @@
+
+from classed.enemy import Enemy
+
+from classed.player import Player
+
+
+player1 = Player("Фордик", "какойтоguyизфнфмода", 100, 100, 50, 10)
+player1.print_infoP()
+
+# player1.say("блин...*cry*")
+
+
+enemy1 = Enemy("Матр4сс","тожегайсфнфмода", 100, 10)
+enemy1.info()
+player1.fight(enemy1)
+# enemy1.info()
+
+
+  
+
+
+# ⁣yes     yes  yes yes yes
+# yesyes  yes  yes     yes
+# yes yes yes  yes     yes
+# yes  yesyes  yes     yes
+# yes     yes  yes yes yes
